983cf00880c00e55dad57d9fbd2e5bc0c75e9191
Mekkeliard <severing8@gmail.com>
2021-01-18 08:49:35 +0300

    Add verification request with parameters

--- ApiChat.Web.Auth/Pages/ApimDelegation.cshtml.cs
+++ ApiChat.Web.Auth/Pages/ApimDelegation.cshtml.cs
@@ -1,14 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace ApiChat.Web.Auth.Pages
 {
     public class ApimDelegationModel : PageModel
     {
+        private string _key;
+
         enum Operations
         {
             SignIn,
@@ -31,11 +36,22 @@ namespace ApiChat.Web.Auth.Pages
             Renew 
         }
 
+        public ApimDelegationModel(IConfiguration configuration)
+        {
+            _key = configuration["Delegation:DelegationValidationKey"];
+        }
+
         public IActionResult OnGet()
         {
             var operation = Enum.Parse<Operations>(Request.Query["operation"].FirstOrDefault());
             var returnUrl = Request.Query["returnUrl"].FirstOrDefault();
-
+#if !DEBUG
+            if (!TryValidation(returnUrl))
+            {
+                return Unauthorized();
+            }
+#endif
+            
             switch (operation)
             {
                 case Operations.SignIn:
@@ -59,6 +75,19 @@ namespace ApiChat.Web.Auth.Pages
             }
 
             return Page();
+        }
+
+        private bool TryValidation(string returnUrl)
+        {
+            string salt = Request.Query["salt"].FirstOrDefault();
+            string sig = Request.Query["sig"].FirstOrDefault();
+            string signature;
+            using var encoder = new HMACSHA512(Convert.FromBase64String(_key));
+
+            signature = Convert.ToBase64String(encoder.ComputeHash(Encoding.UTF8.GetBytes(salt + "\n" + returnUrl)));
+            // change to (salt + "\n" + productId + "\n" + userId) when delegating product subscription
+            // compare signature to sig query parameter
+            return string.Equals(signature, sig, StringComparison.Ordinal);
         }
     }
 }
